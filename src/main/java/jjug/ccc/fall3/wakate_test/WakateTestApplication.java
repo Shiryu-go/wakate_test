cebd92dff9a06612c82ce25072f7baf8df3ca0b5
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-07 07:49:28 +0900

    First Commit

---
+++ src/main/java/jjug/ccc/fall3/wakate_test/WakateTestApplication.java
@@ -0,0 +1,13 @@
+package jjug.ccc.fall3.wakate_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjug/ccc/fall3/wakate_test/WakateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jjug.ccc.fall3.wakate_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
