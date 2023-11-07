333a944701088c140f8fe8f97b141f5d5f5b9fbd
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 00:40:23 +0900

    テストが通るように最低限の実装の追加

--- src/main/java/jjug/ccc/fall3/wakate_test/Controller/ToDoListController.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Controller/ToDoListController.java
@@ -1,4 +1,17 @@
 package jjug.ccc.fall3.wakate_test.Controller;
 
+import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+import jjug.ccc.fall3.wakate_test.Service.ToDoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ToDoListController {
+    @Autowired
+    ToDoListService toDoListService;
+    @GetMapping("/")
+    public ToDoList getToDoList(){
+        return toDoListService.findAll();
+    }
 }
--- src/main/java/jjug/ccc/fall3/wakate_test/Service/ToDoListService.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Service/ToDoListService.java
@@ -1,6 +1,9 @@
 package jjug.ccc.fall3.wakate_test.Service;
 
+import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+
 public class ToDoListService {
-    public void findAll() {
+    public ToDoList findAll() {
+        return null;
     }
 }
