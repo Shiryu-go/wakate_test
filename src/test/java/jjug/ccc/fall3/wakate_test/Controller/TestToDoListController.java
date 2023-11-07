f0d808e626f53d4738ab6175ae151858ca8ee78d
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 01:13:11 +0900

    インポート調整

--- src/test/java/jjug/ccc/fall3/wakate_test/Controller/TestToDoListController.java
+++ src/test/java/jjug/ccc/fall3/wakate_test/Controller/TestToDoListController.java
@@ -7,7 +7,6 @@ import jjug.ccc.fall3.wakate_test.Service.ToDoListService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(ToDoListController.class)
 class TestToDoListController {
 
     @Autowired
