a003e8199ea3f3ab01feb9b545aaefccac8de5f4
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 01:04:52 +0900

    トラブルシューティング @SpringBootTestはどうか

--- src/test/java/jjug/ccc/fall3/wakate_test/Controller/TestToDoListController.java
+++ src/test/java/jjug/ccc/fall3/wakate_test/Controller/TestToDoListController.java
@@ -7,6 +7,7 @@ import jjug.ccc.fall3.wakate_test.Service.ToDoListService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest()
+@SpringBootTest
 class TestToDoListController {
 
     @Autowired
