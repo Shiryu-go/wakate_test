f541f016e5281bddc42dc27be140b52900f765a9
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 01:13:30 +0900

    ServiceがBean登録されていないのが原因だったのではないか。

--- src/main/java/jjug/ccc/fall3/wakate_test/Service/ToDoListService.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Service/ToDoListService.java
@@ -1,7 +1,9 @@
 package jjug.ccc.fall3.wakate_test.Service;
 
 import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ToDoListService {
     public ToDoList findAll() {
         return null;
