d128ed469db18dc7935e31dde47363c9a4718858
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 18:20:12 +0900

    Add : findAll method Serviceクラスのテストが動くようにメソッドの追加

--- src/main/java/jjug/ccc/fall3/wakate_test/Repository/ToDoListRepository.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Repository/ToDoListRepository.java
@@ -1,4 +1,11 @@
 package jjug.ccc.fall3.wakate_test.Repository;
 
+import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ToDoListRepository {
+    public ToDoList findAll() {
+        return null;
+    }
 }
