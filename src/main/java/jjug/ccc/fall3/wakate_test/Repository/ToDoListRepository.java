d31647a6bf901bbe57147e4805ec6a49ea25f9eb
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 18:32:54 +0900

    Add : jdbcTemplateの追加

--- src/main/java/jjug/ccc/fall3/wakate_test/Repository/ToDoListRepository.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Repository/ToDoListRepository.java
@@ -1,10 +1,17 @@
 package jjug.ccc.fall3.wakate_test.Repository;
 
 import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ToDoListRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ToDoListRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public ToDoList findAll() {
         return null;
     }
