1123cbe3fd520eccedd313390941012d85ff404c
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 18:19:40 +0900

    前コミットの変更に伴う修正

--- src/main/java/jjug/ccc/fall3/wakate_test/Record/ToDo.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Record/ToDo.java
@@ -3,6 +3,7 @@ package jjug.ccc.fall3.wakate_test.Record;
 import java.util.Date;
 
 public record ToDo(
+        int ListId,
         int id,
         String ToDo_Title,
         String ToDo_Description,
--- src/main/java/jjug/ccc/fall3/wakate_test/Record/ToDoList.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Record/ToDoList.java
@@ -2,4 +2,4 @@ package jjug.ccc.fall3.wakate_test.Record;
 
 import java.util.List;
 
-public record ToDoList(int id,String title , List<ToDo> toDo){}
+public record ToDoList(int ListId,String title , List<ToDo> toDo){}
