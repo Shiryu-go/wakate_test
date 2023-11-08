8f7d9bad00068c9aade600edc2cabc1b7061561c
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 18:20:44 +0900

    refactor : returnインスタンスの変更 injectMockがうまく動くように

--- src/main/java/jjug/ccc/fall3/wakate_test/Service/ToDoListService.java
+++ src/main/java/jjug/ccc/fall3/wakate_test/Service/ToDoListService.java
@@ -1,11 +1,15 @@
 package jjug.ccc.fall3.wakate_test.Service;
 
 import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+import jjug.ccc.fall3.wakate_test.Repository.ToDoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ToDoListService {
+    @Autowired
+    ToDoListRepository toDoListRepository;
     public ToDoList findAll() {
-        return null;
+        return toDoListRepository.findAll();
     }
 }
