e4a174d25d54298a92b767ff27bdc7759e017db0
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 18:19:27 +0900

    ListIdとTodoのIDを別で分けた。複合ユニークキーになる予定

--- src/test/java/jjug/ccc/fall3/wakate_test/Controller/TestToDoListController.java
+++ src/test/java/jjug/ccc/fall3/wakate_test/Controller/TestToDoListController.java
@@ -35,8 +35,8 @@ class TestToDoListController {
     @Test
     void test_getTodoList()throws Exception{
         List<ToDo> list = new ArrayList<>();
-        ToDo todo1 = new ToDo(1,"白菜","説明を入力",false,new Date(2023/11/7));
-        ToDo todo2 = new ToDo(2,"にんにく","説明を入力",false,new Date(2023/11/7));
+        ToDo todo1 = new ToDo(1,1,"白菜","説明を入力",false,new Date(2023/11/7));
+        ToDo todo2 = new ToDo(1,2,"にんにく","説明を入力",false,new Date(2023/11/7));
         list.add(todo1);
         list.add(todo2);
         ToDoList toDoList = new ToDoList(1,"今日の買い物リスト",list);
