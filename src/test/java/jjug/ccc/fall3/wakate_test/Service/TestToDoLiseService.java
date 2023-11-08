f9390204c249bce67380953060a0af05292ed9ba
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-08 18:18:54 +0900

    Fix MockがInjectされていないインスタンスのメソッドをつかったため、NullpointerExceptionだった様子。

--- src/test/java/jjug/ccc/fall3/wakate_test/Service/TestToDoLiseService.java
+++ src/test/java/jjug/ccc/fall3/wakate_test/Service/TestToDoLiseService.java
@@ -1,15 +1,41 @@
 package jjug.ccc.fall3.wakate_test.Service;
 
 
+import jjug.ccc.fall3.wakate_test.Record.ToDo;
+import jjug.ccc.fall3.wakate_test.Record.ToDoList;
+import jjug.ccc.fall3.wakate_test.Repository.ToDoListRepository;
+import jjug.ccc.fall3.wakate_test.Repository.ToDoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
 public class TestToDoLiseService {
     @InjectMocks
     ToDoListService toDoListService;
+    @Mock
+    ToDoListRepository toDoListRepository;
 
-    @Mock
-    OrderRepository orderRepository;
-    @Mock
-    OrderItemRepository orderItemRepository;
+    @Test
+    void test_findAll(){
+        List<ToDo> list = new ArrayList<>();
+        ToDo todo1 = new ToDo(1,1,"白菜","説明を入力",false,new Date(2023/11/7));
+        ToDo todo2 = new ToDo(1,2,"にんにく","説明を入力",false,new Date(2023/11/7));
+        list.add(todo1);
+        list.add(todo2);
+        ToDoList toDoList = new ToDoList(1,"今日の買い物リスト",list);
+        doReturn(toDoList).when(toDoListRepository).findAll();
+        ToDoList toDoList1 = toDoListService.findAll();
+
+        assertThat(toDoList1.toDo().get(0).ToDo_Title()).isEqualTo("白菜");
+    }
 }
